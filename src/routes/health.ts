550d47f5af03f72c6c342b0a4fe0558550f37762
aliyakundil <aliya1998n@gmail.com>
2026-03-26 00:50:05 +0600

    fix: add timestamp to healthcheck response

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -4,7 +4,7 @@ import type { Request, Response } from "express";
 const router = Router();
 
 router.get('/health', (req: Request, res: Response) => {
-  res.json({ status: "ok", uptime: process.uptime() });
+  res.json({ status: "ok", timestamp: new Date().toISOString()  });
 });
 
 export default router; 
